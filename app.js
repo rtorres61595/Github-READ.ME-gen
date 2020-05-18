e525a73db4d6e668b09c1ba0de9e25ef846d0a1e
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-05-17 20:33:41 -0400

    user error proof my questions

--- app.js
+++ app.js
@@ -25,57 +25,134 @@ function promptUser() {
     {
     type: "input",
     name: "projectTitle",
-    message: "What is the name of your new project?"
+    message: "What is the name of your new project?",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "projectDescrip",
-    message: "Please describe the project?"
+    message: "Please describe the project?",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "tableOfContent",
-    message: "what is your projects table of contents?"
+    message: "what is your projects table of contents?",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "installation",
-    message: "What did you instal?"
+    message: "What did you instal?",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "usage",
-    message: "usage "
+    message: "usage ",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "license",
-    message: "license"
+    message: "license",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
         type: "input",
         name: "contributing",
-        message: "Who will be contributing in the project"
+        message: "Who will be contributing in the project",
+        validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+  
     },
     {
     type: "input",
     name: "test",
-    message: "test"
+    message: "test",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "Questions",
-    message: "questions"
+    message: "questions",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "UserPic",
-    message: "picture"
+    message: "picture",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     {
     type: "input",
     name: "github",
-    message: "Enter your GitHub Username"
+    message: "Enter your GitHub Username",
+    validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+
     },
     
     ]);
